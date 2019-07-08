0ac3c3f8a35d4209f7562cdbf497c6d1dcd4d5e3
Jannah Hossain <jhossain@cbnco.com>
2019-07-08 09:53:41 -0400

    Editing rspec test to determin if it contains database name.

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,7 +1,7 @@
 require ‘spec_helper’
 
 describe 'sync_ts_server' do
-	context 'The following classes should be present in the catalog' do
-		it { should compile }
-	end	
+	do
+     	is_expected.to contain_file('Sync_ts_server::database_name')
+    end
 end
