12131914c06c7eff028ed10d83e034120ce01262
Jannah Hossain <jhossain@cbnco.com>
2019-07-08 13:08:50 -0400

    Added typo for testing

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,7 +1,7 @@
 require ‘spec_helper’
 
 describe 'sync_ts_server' do
-	do
-     	is_expected.to contain_file('Sync_ts_server::database_name')
+    do
+     	is_expected.to contain_file('Sync_ts_server::dataase_name')
     end
 end
