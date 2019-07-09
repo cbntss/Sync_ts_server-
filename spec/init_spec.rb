962a1c0f1d7da3b1d034148040be8aeaae5b0b5b
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:08:44 -0400

    Changed contain to include

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'sync_ts_server' do
-	it { is_expected.to contain('Sync_ts_server::database_name')
+	it { is_expected.to include('Sync_ts_server::database_name')
         .with(
         	:ensure  => 'present',
         	:path    => '/opt/sync_ts_server/config.ini',
