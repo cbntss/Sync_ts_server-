e6c502d58f5f991cc98dea29dc29faa1b97e18fe
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:14:39 -0400

    Moved whats inside include

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe 'sync_ts_server' do
-	it { is_expected.to include('Sync_ts_server::database_name')
-        .with(
+	it { is_expected('Sync_ts_server::database_name').to include(
         	:ensure  => 'present',
         	:path    => '/opt/sync_ts_server/config.ini',
         )
