2fed27bb321ac088b025540ee20bfdb2fefd59df
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 14:27:49 -0400

    Added require puppet fact

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
-require 'rspec-puppet'
+#require 'rspec-puppet'
 
 fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 
 RSpec.configure do |c|
-  c.module_path     = File.join(fixture_path, 'modules')
-  c.manifest_dir    = File.join(fixture_path, 'manifests')
-  c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
+  #c.module_path     = File.join(fixture_path, 'modules')
+  #c.manifest_dir    = File.join(fixture_path, 'manifests')
+  #c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
+  #c.environmentpath = File.join(Dir.pwd, 'spec')
+  c.mock_with :rspec
 end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
