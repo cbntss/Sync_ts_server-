430b440aad398494da47453982fb0d0836399350
Tom Parker <tparker@cbnco.com>
2019-07-09 14:42:42 -0400

    Cleanup facts code that we aren't using.

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 
 describe 'sync_ts_server' do
     it do
-	    is_expected.to contain_ini_setting('Sync_ts_server::database_name').with(
-	      ensure:  'present'
-	    )
-	end
+        is_expected.to contain_ini_setting('sync_ts_server::database_name').with(
+            ensure:  'present'
+        )
+    end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,10 @@
-#require 'rspec-puppet'
+require 'rspec-puppet'
 
 fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 
 RSpec.configure do |c|
-  #c.module_path     = File.join(fixture_path, 'modules')
-  #c.manifest_dir    = File.join(fixture_path, 'manifests')
-  #c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
-  #c.environmentpath = File.join(Dir.pwd, 'spec')
-  c.mock_with :rspec
-end
-
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
-
-if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
-  facts = YAML.load(File.read(File.join(__dir__, 'default_module_facts.yml')))
-  if facts
-    facts.each do |name, value|
-      add_custom_fact name.to_sym, value
-    end
-  end
+  c.module_path     = File.join(fixture_path, 'modules')
+  c.manifest_dir    = File.join(fixture_path, 'manifests')
+  c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
+  c.environmentpath = File.join(Dir.pwd, 'spec')
 end
