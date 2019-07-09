bb4d7ea624c90486af80676811561c00647131ff
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 13:59:27 -0400

    Added contain_ini_setting test

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -2,7 +2,10 @@ require 'pp'
 require 'spec_helper'
 
 describe 'sync_ts_server' do
-    it { is_expected.to contain_ini_settings('Sync_ts_server::database_name') }
+    it do
+	    is_expected.to contain_ini_setting('Sync_ts_server::database_name').with(
+	      ensure:  'present'
+	    )
+	end
 end
 
-pp "Hello World"
