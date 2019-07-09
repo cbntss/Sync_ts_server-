dc08893417dd6518bb676093fe0bdd76754a3554
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:19:09 -0400

    Removed contents inside include

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe 'sync_ts_server' do
-	it { is_expected('Sync_ts_server::database_name').to include(
-        	:ensure  => 'present',
-        	:path    => '/opt/sync_ts_server/config.ini',
-        )
+	it { is_expected('Sync_ts_server::database_name').to include()
      }
 end
