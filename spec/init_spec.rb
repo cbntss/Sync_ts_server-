43a7059f2e0c81f93b53320839e5e3b4df1d48c0
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:53:13 -0400

    emoved test

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
 describe 'sync_ts_server' do
-	it do
-		is_expected.to contain_class('database_name').with({
-        	:ensure  => 'present',
-        	:path    => '/opt/sync_ts_server/config.ini',
-        	:section => 'database',
-        	:setting => 'database',
-        	:value   => 'sync_ts',
-        })
-    end
+	
 end
