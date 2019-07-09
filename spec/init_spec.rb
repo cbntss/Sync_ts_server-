219587017503d43a94d6bf7b447eeb4beda14ab3
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 16:48:34 -0400

    Fixed syntax error

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'sync_ts_server' do
     it do
-        is_expected.to contain_ini_setting('sync_ts_server::database_name').with(
+        is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             ensure:  'present'
         )
     end
