de20ecb878016e51c7b00477f31bebc297f52ee9
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:30:31 -0400

    hanged include to contain_file

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe 'sync_ts_server::database_name' do
-	it { is_expected.to include(
+describe 'sync_ts_server' do
+	it { is_expected.to contain_file('Sync_ts_server::database_name').with_path('/opt/sync_ts_server/config.ini') 
+        .with(
         	:ensure  => 'present',
-        	:path    => '/opt/sync_ts_server/config.ini',
         )
      }
 end
