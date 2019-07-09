9dd631a668072d0bea52f7e612662a6606ef3296
Tom Parker <tparker@cbnco.com>
2019-07-09 12:28:03 -0400

    Check to see if /opt/sync_ts_server/config.ini exists

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,8 +1,12 @@
-require 'spec_helper'
 require 'pp'
+require 'spec_helper'
 
 describe 'sync_ts_server' do
-
+    it do
+        is_expected.to contain_file('/opt/sync_ts_server/config.ini').with({
+            'ensure' => 'present',
+        })
+    end
 end
 
 pp "Hello World"
