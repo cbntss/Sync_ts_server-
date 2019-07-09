b7edd2e1afe368db0bf1217d3c1747eba3f4625e
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 11:24:37 -0400

    Editted describe

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
-describe 'sync_ts_server' do
-	it { is_expected('Sync_ts_server::database_name').to include()
+describe 'sync_ts_server::database_name' do
+	it { is_expected.to include(
+        	:ensure  => 'present',
+        	:path    => '/opt/sync_ts_server/config.ini',
+        )
      }
 end
