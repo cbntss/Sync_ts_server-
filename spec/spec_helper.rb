0db07bde88c6441ae3e37c44a70380ce36e21ae0
Jannah Hossain <jhossain@cbnco.com>
2019-07-09 14:15:35 -0400

    Creating file and added code for puppetlabs configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,16 @@ RSpec.configure do |c|
   c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
   c.environmentpath = File.join(Dir.pwd, 'spec')
 end
+
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
+
+if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
+  facts = YAML.load(File.read(File.join(__dir__, 'default_module_facts.yml')))
+  if facts
+    facts.each do |name, value|
+      add_custom_fact name.to_sym, value
+    end
+  end
+end
