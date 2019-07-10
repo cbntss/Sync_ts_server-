2c9ab1d6aa47d94db03490b1e5d420f9d7a9abed
Jannah Hossain <jhossain@cbnco.com>
2019-07-10 11:43:49 -0400

    Added with function

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -5,7 +5,9 @@ describe 'sync_ts_server' do
         #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             #ensure:  'present'
         #)
-        is_expected.to contain_file('${apache::port_include_dirs}/433.d/sync_ts_server.conf')
+        is_expected.to contain_file('${apache::port_include_dirs}/433.d/sync_ts_server.conf').with(
+    		'ensure'     => 'present',
+  		)
     end
 end
 
