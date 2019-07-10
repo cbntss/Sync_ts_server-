2352593493ff3d9ef5623233639fc6a4079032fa
Jannah Hossain <jhossain@cbnco.com>
2019-07-10 12:55:54 -0400

    Added contain_class

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,13 +1,11 @@
 require "spec_helper"
 
 describe 'sync_ts_server' do
-    it do
+    #it do
         #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             #ensure:  'present'
         #)
-        is_expected.to contain_file('${apache::port_include_dirs}/433.d/sync_ts_server.conf').with(
-    		'ensure'     => 'present',
-  		)
-    end
+        it { is_expected.to contain_class('sync_ts_server') }
+    #end
 end
 
