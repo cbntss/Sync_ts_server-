4e1f55c20eff939bd9f994b993d9ebdd44080c9e
Jannah Hossain <jhossain@cbnco.com>
2019-07-10 14:43:50 -0400

    removed older version of ruby and added test to check if the files respond

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,10 +1,11 @@
 require "spec_helper"
+require "pp"
 
 describe 'sync_ts_server' do
     it do
         is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
-            ensure:  'present'
+            ensure:  'non'
         )
     end
 end
-
+pp "hello world"
