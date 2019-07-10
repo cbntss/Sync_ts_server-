73f45485430c7b10c0376e764e3f458867381b19
Jannah Hossain <jhossain@cbnco.com>
2019-07-10 14:39:23 -0400

    Added ini_setting test, and set current puppet version in yml

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,11 +1,10 @@
 require "spec_helper"
 
 describe 'sync_ts_server' do
-    #it do
-        #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
-            #ensure:  'present'
-        #)
-        it { is_expected.to contain_class('sync_ts_server') }
-    #end
+    it do
+        is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
+            ensure:  'present'
+        )
+    end
 end
 
