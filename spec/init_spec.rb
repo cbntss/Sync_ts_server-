d072a61356366b90a7d93ed6f84defd2964ef12c
Jannah Hossain <jhossain@cbnco.com>
2019-07-10 10:30:38 -0400

    Created a file test

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe 'sync_ts_server' do
     it do
-        is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
-            ensure:  'present'
-        )
+        #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
+            #ensure:  'present'
+        #)
+        is_expected.to contain_file('${apache::port_include_dirs}/433.d/sync_ts_server.conf')
     end
 end
 
