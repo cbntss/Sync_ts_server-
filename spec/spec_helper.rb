82f36a74bfdde650d9b2dee1a5812e21e91fe862
Jannah Hossain <jhossain@cbnco.com>
2019-07-11 12:33:00 -0400

    Added a config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec-puppet'
 fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 
 RSpec.configure do |c|
+  c.mock_with :rspec
   c.module_path     = File.join(fixture_path, 'modules')
   c.manifest_dir    = File.join(fixture_path, 'manifests')
   c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
