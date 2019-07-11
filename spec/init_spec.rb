5a6a7a83e87b0febd3918d65686462740d35ecf4
Jannah Hossain <jhossain@cbnco.com>
2019-07-11 14:18:30 -0400

    Added compile test

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -3,11 +3,7 @@ require "pp"
 
 describe 'sync_ts_server' do
 	pp "Hello"
-    it do
-        #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
-            #ensure:  'present'
-        #)
-        is_expected.to contain_class("sync_ts_server")
-   end
+
+	it { is_expected.to compile }
 end
 
