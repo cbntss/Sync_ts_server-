83701b6bd80a3feceb6abe49019b564c7e0b8095
Jannah Hossain <jhossain@cbnco.com>
2019-07-11 11:58:40 -0400

    Added require pp to test

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,11 +1,13 @@
 require "spec_helper"
+require "pp"
 
 describe 'sync_ts_server' do
+	pp "Hello"
     #it do
         #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             #ensure:  'present'
         #)
-        it { is_expected.to contain_class('sync_ts_server') }
+        #it { is_expected.to contain_class('sync_ts_server') }
    #end
 end
 
