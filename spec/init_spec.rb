64e3d266610795be63cddcbfe21aa6908f57ec20
Jannah Hossain <jhossain@cbnco.com>
2019-07-11 14:38:22 -0400

    Fixed syntax error

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,9 +1,8 @@
 require "spec_helper"
 require "pp"
 
-describe 'sync_ts_server' , :type => :class do
+describe 'sync_ts_server', :type => :class do
 	pp "Hello"
 	it { is_expected.to contain_class('sync_ts_server') }
-	
 end
 
