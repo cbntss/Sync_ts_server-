8ced97ec39a398652dcaed324ab6c7b7e713d419
Jannah Hossain <jhossain@cbnco.com>
2019-07-11 11:53:27 -0400

    Commented out code

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
-require "pp"
 
 describe 'sync_ts_server' do
-    it do
-        is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
-            ensure:  'present'
-        )
-    end
+    #it do
+        #is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
+            #ensure:  'present'
+        #)
+        it { is_expected.to contain_class('sync_ts_server') }
+   #end
 end
-pp "hello world"
+
