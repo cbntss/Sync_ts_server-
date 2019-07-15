34b2041e139f4e48323547b2cb6f4859a31ce49f
Jannah Hossain <jhossain@cbnco.com>
2019-07-15 10:19:16 -0400

    Added class type

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -1,14 +1,8 @@
 require "spec_helper"
 require "pp"
 
-describe 'sync_ts_server' do
-	pp "Hello"
-	it { should compile}
+describe 'sync_ts_server', type: :class do
 
-	it do
-	  is_expected.to contain_file('${apache::port_include_dirs}/443.d/sync_ts_server.conf').with(
-	    'ensure'     => 'file'
-	  )
-	end
+	it { should compile}
 end
 
