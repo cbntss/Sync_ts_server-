347f2a0e8559d5d5009158a185c5cb5f28bc2272
Jannah Hossain <jhossain@cbnco.com>
2019-07-15 08:49:27 -0400

    Created test to check for file

--- spec/init_spec.rb
+++ spec/init_spec.rb
@@ -4,5 +4,11 @@ require "pp"
 describe 'sync_ts_server' do
 	pp "Hello"
 	it { is_expected.to compile}
+
+	it do
+	  is_expected.to contain_file('${apache::port_include_dirs}/443.d/sync_ts_server.conf').with(
+	    'ensure'     => 'file'
+	  )
+	end
 end
 
