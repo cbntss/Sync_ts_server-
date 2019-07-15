c9f1b1b59aed2e2693baaeeaa309ed2b6d7fddc3
Jannah Hossain <jhossain@cbnco.com>
2019-07-15 12:41:03 -0400

    Changed require statment

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
-require "spec_helper"
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 describe 'sync_ts_server' do
-	context 'with default values for all parameters' do 
+	context 'with default values for all parameters' do
 		it { should contain_class('sync_ts_server')}
 	end
 end
