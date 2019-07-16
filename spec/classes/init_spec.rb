9b74eaa0c00801d386ab1083f888854b9ba395cd
Jannah Hossain <jhossain@cbnco.com>
2019-07-16 10:15:19 -0400

    Changed test to test file

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 describe "sync-ts-server" do
-        it { is_expected.to contain_class('sync-ts-server')}
+    it do
+        it { is_expected.to contain_file('${apache::port_include_dirs}/443.d/sync_ts_server.conf')}
+    end
 end
 
