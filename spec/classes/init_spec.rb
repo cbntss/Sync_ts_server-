5965ca54ef12dc4a21d5b2d14a6353c5ff08897f
Jannah Hossain <jhossain@cbnco.com>
2019-07-16 09:54:05 -0400

    Fixed syntax error

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 describe 'sync-ts-server' do
-    it  do
+
+   let do
         it { is_expected.to  contain_class('sync-ts-server')}
     end
 end
