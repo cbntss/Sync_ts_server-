7bcadf4337d1e3971042ed2a06f06d9aa5dfd53d
Jannah Hossain <jhossain@cbnco.com>
2019-07-17 11:30:02 -0400

    Added ini_setting test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe "sync_ts_server" do
-    it { is_expected.to compile }
+    it do
+        is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
+            ensure:  'present'
+        )
+    end
 end
 
