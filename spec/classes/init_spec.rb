2acc9feed75c38805f90c15bae0d74d3a359e0ed
Jannah Hossain <jhossain@cbnco.com>
2019-07-17 11:13:39 -0400

    Added compile test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe "sync_ts_server" do
-    context 'with ensure => present' do
-        let(:params) { {'ensure' => 'present'} }
-
-        it { is_expected.to compile }
-  end
+    it { is_expected.to compile }
 end
 
