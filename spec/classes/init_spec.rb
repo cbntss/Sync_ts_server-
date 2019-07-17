d8d50b5ae3e4f60107377d5a8ec042b31e6d5735
Jannah Hossain <jhossain@cbnco.com>
2019-07-17 11:48:26 -0400

    Added parameters

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe "sync_ts_server" do
     it do
+        let(:params) { {'packages' => 'pac'} }
+        let(:params) { {'db_password' => '_password_'} }
+        let(:params) { {'terminal_url' => 'url'} }
+
+        it { is_expected.to compile }
         is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             ensure:  'present'
         )
