babd9137e3ada58d81c1da22dd85024f0307d942
Jannah Hossain <jhossain@cbnco.com>
2019-07-17 11:56:04 -0400

    Fixed syntax error

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe "sync_ts_server" do
     it do
-        let(:params) { {'packages' => 'pac'} }
-        let(:params) { {'db_password' => '_password_'} }
-        let(:params) { {'terminal_url' => 'url'} }
+        context 'setting package' do
+            let(:params) { {'packages' => 'pac'} }
+        end
+        context 'setting password' do
+             let(:params) { {'db_password' => '_password_'} }
+        end
+        context 'setting url' do
+            let(:params) { {'terminal_url' => 'url'} }
+        end
+
 
         it { is_expected.to compile }
         is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
