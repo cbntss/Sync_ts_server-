4193c1042d63e96df17a065543e8f476b1668069
Tom Parker <tparker@cbnco.com>
2019-07-19 13:00:20 -0400

    Added require: puppetlabs_spec_helper/puppet_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ RSpec.configure do |c|
   c.mock_with :rspec
 end
 
-require 'puppetlabs_spec_helper/module_spec_helper'
+require 'puppetlabs_spec_helper/puppet_spec_helper'
+#require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
 fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
