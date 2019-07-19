701cc7995825de07ad837d442e20cf55b26a646c
Tom Parker <tparker@cbnco.com>
2019-07-19 13:55:07 -0400

    Defined apache spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
+describe "apache" do
+    let(:params) {
+        {
+            'default_modules' => 'ssl',
+            'disclaimer' => 'This is a private system provided for authorized uses only. All other access is strictly prohibited.',
+            'manage_vhosts' => 'yes',
+            'ssl_cipher_suites' => 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256'
+        }
+    }
+
+    it { is_expected.to compile }
+end
+
 describe "sync_ts_server" do
+    let(:params) {
+        {
+            'packages' => ['pac'],
+            'db_password' => '_password_',
+            'terminal_url' => 'url',
+        }
+    }
 
-     let(:params) { {'packages' => ['pac'], 'db_password' => '_password_', 'terminal_url' => 'url', 'default_modules' => 'ssl', 'disclaimer' => 'This is a private system provided for authorized uses only. All other access is strictly prohibited.', 'manage_vhosts' => 'yes', 'ssl_cipher_suites' => 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256'} }
+    it { is_expected.to compile }
 
-        it { is_expected.to compile }
     it do
         is_expected.to contain_ini_setting('sync_ts_server::database_username').with(
             ensure:  'present'
