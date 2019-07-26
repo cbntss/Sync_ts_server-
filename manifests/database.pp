83ced02f378f4ab607fa1c5e1da8cfcc1b565745
Jannah Hossain <jhossain@cbnco.com>
2019-07-26 14:14:20 -0400

    Added database, and variables

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,16 +1,18 @@
 class sync_ts_server::database (
+    String $sync_ts_dbname='test_two_sync_ts',
     String $user_password='test',
     String $username='testuser',
 ) {
     contain postgresql::server
 
-    postgresql::server::db { 'test_two_sync_ts':
-        user     => 'testuser',
-        password => postgresql_password('testuser', 'test'),
-        owner    => 'testuser',
+    postgresql::server::db { $sync_ts_dbname:
+        user     => $username,
+        password => postgresql_password($username, $user_password),
+        owner    => $username,
     }
 
-   postgresql_psql { 'schema-1.0':
+   postgresql_psql { 'schema_sql':
+        db => $sync_ts_dbname,
         command => file("${module_name}/schema-1.0.sql"),
    }
 }
