60d57f847ff47f70f81c0567393a2b07108a77d4
Tom Parker <tparker@cbnco.com>
2019-08-09 12:24:30 -0400

    Code cleanup for the database module.

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,23 +1,24 @@
+# Manage the sync_ts_server database
+
 class sync_ts_server::database (
     String $sync_ts_dbname='sync_ts',
     String $user_password='sync_ts',
     String $username='sync_ts',
     String $schemaname = 'public',
     String $dbversion_table = 'dbversion',
-
 ) {
     contain postgresql::server
 
-    postgresql::server::db { $sync_ts_dbname:
+    postgresql::server::db {$sync_ts_dbname:
         user     => $username,
         password => postgresql_password($username, $user_password),
         owner    => $username,
     }
 
-   postgresql_psql { 'schema_sql':
-        db       => $sync_ts_dbname,
-        unless   => "select * from pg_tables where schemaname = '${schemaname}' and tablename = '${dbversion_table}'",
-        command  => file("${module_name}/schema-1.0.sql"),
+    postgresql_psql {'schema_sql':
+        db      => $sync_ts_dbname,
+        unless  => "select * from pg_tables where schemaname = '${schemaname}' and tablename = '${dbversion_table}'",
+        command => file("${module_name}/schema-1.0.sql"),
         require => Postgresql::Server::Db[$sync_ts_dbname],
-   }
+    }
 }
