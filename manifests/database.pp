817b5470995dfc3d53bfc045b6aba0c0e598c6c2
Tom Parker <tparker@cbnco.com>
2019-08-09 14:44:36 -0400

    Change variable to create_database to skip the postgres db creation steps.  It's too tightly coupled to managaged postgres

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,43 +1,25 @@
 # Manage the sync_ts_server database
 
 class sync_ts_server::database (
+    Boolean $create_database = true,
     String $dbversion_table = 'dbversion',
     Optional[String] $encoding = undef,
     Optional[String] $locale = undef,
-    Boolean $manage_postgres = true,
     String $schemaname = 'public',
     String $sync_ts_dbname = 'sync_ts',
     String $username = 'sync_ts',
     String $userpass = 'sync_ts',
 ) {
-    if ($manage_postgres == true){
+    if ($create_database == true){
         contain postgresql::server
-    }else{
-        notify {"Not managing postgres.": }
-    }
 
-    if (defined('$postgresql::server::encoding')) and ($encoding == undef){
-        # If we don't set encoding but postgresql does, use theirs.
-        $db_encoding = $postgresql::server::encoding
-    }else{
-        # Otherwise use ours (or undef)
-        $db_encoding = $encoding
-    }
-
-    if (defined('$postgresql::server::locale')) and ($locale == undef){
-        # If we don't set locale but postgresql does, use theirs.
-        $db_locale = $postgresql::server::locale
-    }else{
-        # Otherwise use ours (or undef)
-        $db_locale = $locale
-    }
-
-    postgresql::server::db {$sync_ts_dbname:
-        encoding    => $db_encoding,
-        locale      => $db_locale,
-        owner       => $username,
-        password    => postgresql_password($username, $userpass),
-        user        => $username,
+        postgresql::server::db {$sync_ts_dbname:
+            encoding    => $db_encoding,
+            locale      => $db_locale,
+            owner       => $username,
+            password    => postgresql_password($username, $userpass),
+            user        => $username,
+        }
     }
 
     postgresql_psql {'schema_sql':
