220c506ae7372433c9d9322bb2694c139391be8a
Tom Parker <tparker@cbnco.com>
2019-08-09 13:38:46 -0400

    Default to the settings of postgresql::server::encoding and postgresql::server::locale if they are set

--- manifests/database.pp
+++ manifests/database.pp
@@ -16,12 +16,28 @@ class sync_ts_server::database (
         notify {"Not managing postgres.": }
     }
 
+    if (isset('postgresql::server::encoding')) and ($encoding == undef){
+        # If we don't set encoding but postgresql does, use theirs.
+        $db_encoding = $postgresql::server::encoding
+    }else{
+        # Otherwise use ours (or undef)
+        $db_encoding = $encoding
+    }
+
+    if (isset('postgresql::server::locale')) and ($locale == undef){
+        # If we don't set locale but postgresql does, use theirs.
+        $db_locale = $postgresql::server::locale
+    }else{
+        # Otherwise use ours (or undef)
+        $db_locale = $locale
+    }
+
     postgresql::server::db {$sync_ts_dbname:
-        encoding    => $encoding,
-        locale      => $locale,
-        user        => $username,
-        password    => postgresql_password($username, $userpass),
+        encoding    => $db_encoding,
+        locale      => $db_locale,
         owner       => $username,
+        password    => postgresql_password($username, $userpass),
+        user        => $username,
     }
 
     postgresql_psql {'schema_sql':
